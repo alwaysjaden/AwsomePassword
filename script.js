28e206789cddce98ecdd2310b9fffc8eac37d1a4
Jaden Lee <alwaysjaden@gmail.com>
2020-10-24 21:16:37 -0700

    data/input validation via while loop

--- script.js
+++ script.js
@@ -7,20 +7,15 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-
 // Stings 
-
 var passWordkey =[]
 var password=[]
 var randomCount=[]
-
 
 //  Confriming Promts 
 
@@ -28,31 +23,30 @@ var passWordLength = prompt ( " How Long is your password choose between 8 - 128
 
 
 // add If statment regarding Type of Characters  with 4 Trys
-for ( var k=0 ; k<4; k++){
-    if (passWordLength<8 || passWordLength>128) {
+    while (passWordLength<8 || passWordLength>128) {
+        var passWordLength = prompt ( " How Long is your password choose between 8 - 128 Characters" );
         alert("You need to select length of your password to be between 8 - 128 digits");
-        var passWordLength= prompt("Please re-enter lenght of your password")
-    } }
-
-
+        
+    } 
 // Choose elements to be mixed 
-
 var lower=["a","b","c","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upper=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var number=["1","2","3","4","5","6","7","8","9","0"];
 var special=["!","@","#","$","%","^","&","*","_","+","-","=","?","/"];
+
+//  Create Intiger Pool based on Confirmed Value  
+
+
+while (passWordkey.length<1) {
 
 var addLower= confirm( "Do you need Lower Case letter in Your Password?");
 var addUpper= confirm(" Do you need Upper Case Letter in your Password?");
 var addNumber= confirm(" Do you need Number in your Password?");
 var addSepcial= confirm(" Do you need Secial Character in your Password?");
 
-//  Create Intiger Pool based on Confirmed Value  
-
 if (addLower === true) {
     passWordkey.push(lower);
-    }
-    
+    }    
 if (addUpper === true) {
     passWordkey.push(upper);
     }
@@ -62,6 +56,11 @@ if (addNumber === true) {
 if (addSepcial === true) {
     passWordkey.push(special);
     }
+
+if (passWordkey.length === 0) {
+        alert( "Please Restart Awsome Password Generator, You need to choose at least one type of Characters to Generate Password");
+    }
+}
 
 //create random arry with arry of arrays using 2 random variables
 
@@ -73,8 +72,6 @@ for (var i = 0; i < passWordLength; i++){
 
   password.push (passWordkey[randomCount][randomCount2]);
 }
-
-
 // Create New Array for password and join all arrays into one to eliminate " , " when display on document 
 
 password=password.join('');
